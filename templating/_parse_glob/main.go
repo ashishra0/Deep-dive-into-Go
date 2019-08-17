edf797b2a0d89370eaa00eee607861a8ddd1edde
Ashish Rao <ashishrao2598@gmail.com>
2019-08-17 14:56:49 +0530

    make program performant with init function and parseGlob method

---
+++ templating/_parse_glob/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template // assigned for global scope. So this is accessible throughout the package.
+
+func init() { // This function runs once when the program is starting up.
+	tpl = template.Must(template.ParseGlob("templates/*")) // template.Must will do the error checking on the pointer to template and return the pointer to template
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "three.txt", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
